27141021e0c9272d9c22ee7e99f39cc8be4c7a0d
Halison Patrick Xavier <halison.xavier@rxbh03.com.br>
2019-01-16 13:12:52 -0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+window.addEventListener("load", () => {
+    let skills = document.querySelectorAll("[data-level]");
+
+    const levelFactory = (level) => {
+        let aux = "";
+
+        for (i = 0; i < Math.floor(level); i++) {
+            aux += "<i class='fas fa-star' style='font-size: 12px'></i>"
+        }
+        if (level - Math.floor(level) != 0) {
+            aux += "<i class='fas fa-star-half' style='font-size: 12px'></i>"
+        }
+        return aux;
+    }
+
+    Object.entries(skills).forEach((entry) => {
+        entry[1].innerHTML = levelFactory(entry[1].dataset.level)
+    });
+});
